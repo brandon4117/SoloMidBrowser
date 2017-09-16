a2aa2d23d9aea79288bfdab809ee9f063116cd68
digital-vitality <bloehlein@yahoo.com>
2017-09-15 20:46:52 -0500

    Update imgSwap.js

--- imgSwap.js
+++ imgSwap.js
@@ -1,4 +1,4 @@
-var bImages = ['https://raw.githubusercontent.com/digital-vitality/solomidbrowser/master/WebsiteView.png', 'https://digital-vitality.github.io/solomidbrowser/AppScrSht.png'];
+var bImages = ['WebsiteView', 'https://digital-vitality.github.io/solomidbrowser/AppScrSht.png'];
 
  var imageSwap = (function() {
    var i = 0;
