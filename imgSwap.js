6c9716e1fd41cff0c895246b6beb1c48a3978eab
digital-vitality <bloehlein@yahoo.com>
2017-09-22 12:11:40 -0500

    Add files via upload

--- imgSwap.js
+++ imgSwap.js
@@ -1,4 +1,4 @@
-var bImages = ['mainPage.png', 'GameLibrary.png', 'lolView.png'];
+var bImages = ['mainPage.png', 'GameLibrary.png', 'lolView.png', 'bioPage.png', 'streamPage.png'];
 
 var cycle_Imgs = function()
 {
